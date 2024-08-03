fbdf8458dbccf91f4b21a757437857214eec7704
Yousef ayman <yousefayman2000500@gmail.com>
2024-08-03 14:47:42 +0300

    validate data

--- script.js
+++ script.js
@@ -3,6 +3,50 @@
 const inputElements = document.querySelectorAll(".card__input");
 const submitButton = document.querySelector(".card__button");
 
+const validateDay=(day)=>{
+    if(day && day > 0 && day <= 31){
+        return true
+    }
+}
+const validateMonth=(month)=>{
+    if(month && month > 0 && month <= 12){
+        return true
+    }
+}
+const validateYear=(year)=>{
+    const currentYear = new Date().getFullYear()
+    if(year && year > 0 && year <= currentYear){
+        return true
+    }
+}
+const isDataValid =(dayElement,monthElement,yearElement)=>{
+    const isValid = [false,false,false];
+
+    if(!validateDay(dayElement.value)){
+        dayElement.classList.add("card__input--error")
+    }else {
+        isValid[0]=true;
+        dayElement.classList.remove("card__input--error")
+    }
+
+    if(!validateMonth(monthElement.value)){
+        monthElement.classList.add("card__input--error")
+    }else {
+        isValid[1]=true;
+        monthElement.classList.remove("card__input--error")
+    }
+
+    if(!validateYear(yearElement.value)){
+        yearElement.classList.add("card__input--error")
+    }else {
+        isValid[2]=true;
+        yearElement.classList.remove("card__input--error")
+    }
+    return isValid.every(item => item ===true)
+}
+
+
+
 
 const onClickHandler = ()=>{
     const dayElement = document.querySelector(".card__input[name ='day']");
@@ -10,6 +54,10 @@ const onClickHandler = ()=>{
     const yearElement = document.querySelector(".card__input[name ='year']");
     const ageLabel =document.querySelector(".card__resultValue");
 
+    if(!isDataValid(dayElement,monthElement,yearElement)){
+        ageLabel.textContent = "--"
+        return;
+    }
     ageLabel.textContent = calculateAge(yearElement.value,monthElement.value,dayElement.value)   
 }
 const calculateAge =(year,month,day)=>{
@@ -32,3 +80,6 @@ const calculateAge =(year,month,day)=>{
 }
 
 submitButton.addEventListener("click",onClickHandler)
+inputElements.forEach((el)=>{
+    el.addEventListener("keypress",event=>event.key ==="Enter" && onClickHandler())
+})
